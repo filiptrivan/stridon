430b91a39e22211d918efbe0e671606def4f51fd
filiptrivan <120225885+filiptrivan@users.noreply.github.com>
2023-04-30 22:14:05 +0200

    jos zajebancije sa tsx

--- pages/blog/[slug]/components/BlogDetails.tsx
+++ pages/blog/[slug]/components/BlogDetails.tsx
@@ -1,27 +1,43 @@
 import React from "react";
-import Records from "../../blogovi.json";
 
-const BlogDetails = () => {
+export interface BlogData {
+  title: string;
+  main_img: string;
+  created_time: string;
+  description: string;
+  vrsta: {
+    title: string;
+  };
+}
+
+interface Props {
+  blogData: BlogData[];
+}
+
+const BlogDetails: React.FC<Props> = ({ blogData }) => {
   return (
     <div>
-      {Records.filter(
+      {blogData
+      .
+      filter(
         //record.index == index promeni kasnije
-        (record) => record.title == "Introduction to JavaScript"
-      ).map((record) => (
+        (record) => record.title == "Bosch je najbolji"
+      )
+      .map((record) => (
         <div>
           <div>
             <img
-              src={record.image}
+              src={record.main_img}
               alt=""
               className="w-full max-h-[400px] object-cover px-3"
             />
           </div>
 
-          <div className="max-w-[760px] mx-auto px-3">
-            <div className="text-right py-2 mb-3">{record.date}</div>
+          <div className="max-w-[760px] mx-auto px-3 mb-20">
+            <div className="text-right py-2 mb-3">{record.created_time}</div>
             <div className="text-4xl mb-2 font-medium">{record.title}</div>
-            <div>{record.type}</div>
-            <div className="mt-10">{record.text}</div>
+            <div>{record.vrsta.title}</div>
+            <div className="mt-10">{record.description}</div>
           </div>
         </div>
       ))}
--- pages/blog/[slug]/index.tsx
+++ pages/blog/[slug]/index.tsx
@@ -1,13 +1,32 @@
 
 import React from 'react'
-import BlogDetails from './components/BlogDetails'
+import BlogDetails, {BlogData} from './components/BlogDetails'
+import {prisma} from "../../../server/db/client"
 
-const index = () => {
+interface IndexProps {
+  blogs: BlogData[]
+}
+
+const index: React.FC<IndexProps> = ({ blogs }) => {
   return (
     <div className='max-w-[1140px] mx-auto '>
-      <BlogDetails/>
+      <BlogDetails blogData = {blogs}/>
     </div>
   )
 }
 
 export default index
+
+export async function getServerSideProps() {
+  const blogs = await prisma.blog.findMany({
+    include: {
+      vrsta: true,
+    },
+  });
+
+  return {
+    props: {
+      blogs: JSON.parse(JSON.stringify(blogs)),
+    },
+  };
+}
