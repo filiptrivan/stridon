7da6df96315eb0ae36b7fe3929b8e3a548a99fc9
filiptrivan <120225885+filiptrivan@users.noreply.github.com>
2025-08-07 01:54:08 +0200

    Update 404 error messages and remove border style
    Revised the 'NotFoundErrorMessage' in both English and Serbian locale files for clarity and accuracy.
    
    Removed the border style from the error page container in _error.tsx.

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -6,8 +6,7 @@ function ErrorPage() {
 
   return (
     <div>
-      <div className="max-w-[1140px] m-auto py-24 my-10 border-2 p-16 text-center">
-        {/* naslov */}
+      <div className="max-w-[1140px] m-auto py-24 my-10 p-16 text-center">
         <div className="text-3xl font-medium py-24">
           {translate("NotFoundErrorMessage")}
         </div>
