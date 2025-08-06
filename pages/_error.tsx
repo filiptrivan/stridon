96619ebabc95cb69a5d0bcc25e3924a2525a9721
A-Sal3 <asalinas3205@gmail.com>
2025-08-06 12:09:33 -0500

    Translated 404 page

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,10 +1,15 @@
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
 function ErrorPage() {
+  const { t: translate } = useTranslation("home");
+
   return (
     <div>
       <div className="max-w-[1140px] m-auto py-24 my-10 border-2 p-16 text-center">
         {/* naslov */}
         <div className="text-3xl font-medium py-24">
-          Došlo je do greške, molimo Vas kontaktirajte podršku
+          {translate("Došlo je do greške, molimo Vas kontaktirajte podršku")}
         </div>
       </div>
     </div>
@@ -12,3 +17,10 @@ function ErrorPage() {
 }
 
 export default ErrorPage;
+export async function getStaticProps({locale}:any) {
+  return{
+    props:{
+      ...(await serverSideTranslations(locale,["home"]))
+    }
+  }
+}
