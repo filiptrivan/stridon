5c6b1fb30262380f9ac243efa009132f60491579
Adrian Salinas <asalinas3205@gmail.com>
2025-08-08 05:02:56 -0500

    Updated About Us to be consistent with other pages (#18)
    * Updated About Us header to be consistant with other pages
    
    * Updated padding and add alternating background color

--- pages/onama/index.tsx
+++ pages/onama/index.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Head from "next/head";
 import Mapa from "@/components/Mapa";
+import Hero from "@/components/Hero";
 
 const index = () => {
   const { t: translate } = useTranslation("home");
@@ -16,10 +17,13 @@ const index = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="text-3xl sm:text-5xl font-medium text-center pb-12 pt-12">
-        {translate("O nama")}
-      </h1>
-      <div className="max-w-[1140px] mx-auto p-3 flex flex-col-reverse lg:flex-row mb-10 sm:mb-20">
+
+      <Hero
+        translate={translate}
+        titleNaHomePage={translate("O nama")}
+        opisNaHomePage={translate("AboutUsDescription")}
+      />
+      <div className="max-w-[1140px] py-20 px-4 mx-auto flex flex-col-reverse lg:flex-row">
         <div className="max-w-full lg:max-w-[570px] lg:mb-0">
           <Image
             priority
@@ -30,7 +34,7 @@ const index = () => {
             height={1080}
           />
         </div>
-        <div className="max-w-full lg:max-w-[570px] lg:pl-10 mb-4">
+        <div className="max-w-full lg:max-w-[570px] lg:pl-10">
           <h2 className="sm:text-3xl text-2xl font-medium">
             {translate("Osnivanje")}
           </h2>
@@ -42,30 +46,31 @@ const index = () => {
           </p>
         </div>
       </div>
-
-      <div className="max-w-[1140px] mx-auto p-3 flex flex-col lg:flex-row mb-10 sm:mb-20">
-        <div className="max-w-full lg:max-w-[570px] lg:pr-10 mb-4">
-          <h2 className="sm:text-3xl text-2xl font-medium">
-            {translate("Veleprodaja i maloprodaja")}
-          </h2>
-          <span className="separator"></span>
-          <p>
-            {" "}
-            {translate("tekst 2 o nama")}
-            {/* {" "} */}
-          </p>
-        </div>
-        <div className="max-w-full lg:max-w-[570px]">
-          <Image
-            src="/altina1.png"
-            alt="Stridon altina maloprodaja"
-            className="rounded-md w-full"
-            width={1920}
-            height={1080}
-          />
+      <div className="w-full bg-stone-50 ">
+        <div className="max-w-[1140px] py-20 px-4 mx-auto flex flex-col lg:flex-row">
+          <div className="max-w-full lg:max-w-[570px] lg:pr-10">
+            <h2 className="sm:text-3xl text-2xl font-medium">
+              {translate("Veleprodaja i maloprodaja")}
+            </h2>
+            <span className="separator"></span>
+            <p>
+              {" "}
+              {translate("tekst 2 o nama")}
+              {/* {" "} */}
+            </p>
+          </div>
+          <div className="max-w-full lg:max-w-[570px]">
+            <Image
+              src="/altina1.png"
+              alt="Stridon altina maloprodaja"
+              className="rounded-md w-full"
+              width={1920}
+              height={1080}
+            />
+          </div>
         </div>
       </div>
-      <div className="max-w-[1140px] mx-auto p-3 flex flex-col-reverse lg:flex-row mb-10">
+      <div className="max-w-[1140px] py-20 px-4 mx-auto flex flex-col-reverse lg:flex-row">
         <div className="max-w-full lg:max-w-[570px] lg:mb-0">
           <Image
             priority
@@ -76,7 +81,7 @@ const index = () => {
             height={1080}
           />
         </div>
-        <div className="max-w-full lg:max-w-[570px] lg:pl-10 mb-4">
+        <div className="max-w-full lg:max-w-[570px] lg:pl-10">
           <h2 className="sm:text-3xl text-2xl font-medium">
             {translate("Internet prodavnica")}
           </h2>
@@ -88,13 +93,15 @@ const index = () => {
           </p>
         </div>
       </div>
-      <Mapa
-        translate={translate}
-        email={"office@stridon.rs"}
-        kontakt={"011/2886-509"}
-        adresa={"Vojislava Ilića 141g"}
-        map_src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d90567.5103257972!2d20.365943012636272!3d44.81678309583739!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475a7163a682044d%3A0x2a07a073e49f36ae!2sStridon%20group!5e0!3m2!1sen!2snl!4v1682522248790!5m2!1sen!2snl"
-      />
+      <div className="bg-stone-50">
+        <Mapa
+          translate={translate}
+          email={"office@stridon.rs"}
+          kontakt={"011/2886-509"}
+          adresa={"Vojislava Ilića 141g"}
+          map_src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d90567.5103257972!2d20.365943012636272!3d44.81678309583739!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475a7163a682044d%3A0x2a07a073e49f36ae!2sStridon%20group!5e0!3m2!1sen!2snl!4v1682522248790!5m2!1sen!2snl"
+        />
+      </div>
       <Mapa
         translate={translate}
         email={"office@stridon.rs"}
