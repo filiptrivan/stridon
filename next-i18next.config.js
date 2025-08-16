9f6f18a5dc1c395834f2fb22536bc79be20eccf2
A-Sal3 <asalinas3205@gmail.com>
2025-08-16 12:46:20 -0500

    Translation: Sparta Page

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     defaultLocale: "sr",
     localeDetection: false,
     ns: ["common", "home", "error", "kontakt", "b2b", "katalozi", "onama", "podacizaidentifikaciju", "politikaprivatnosti", "servis",
-      "brendovi", "black-and-decker", "bosch", "dewalt", "gtv", "hogert", "karcher", "knipex", "kwb", "max", "mtx", "rem", "rubi", "senco", "sg-tools",
+      "brendovi", "black-and-decker", "bosch", "dewalt", "gtv", "hogert", "karcher", "knipex", "kwb", "max", "mtx", "rem", "rubi", "senco", "sg-tools", "sparta",
       "FormaZaPopunjavanje", "kataloziCard", "brendoviCard", "NavBar", "Mapa", "Footer"],
   },
   interpolation: {
